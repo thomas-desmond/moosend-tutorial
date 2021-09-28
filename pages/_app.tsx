51cc6e3e2e776f85750dc27010d05dc65d3b3fe6
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-09-28 15:51:08 -0700

    Add moosend custom install code to app wide <Head>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>      
+        <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            //load TrackerJS
+            !function(t,n,e,o,a){function d(t){var n=~~(Date.now()/3e5),o=document.createElement(e);o.async=!0,o.src=t+"?ts="+n;var a=document.getElementsByTagName(e)[0];a.parentNode.insertBefore(o,a)}t.MooTrackerObject=a,t[a]=t[a]||function(){return t[a].q?void t[a].q.push(arguments):void(t[a].q=[arguments])},window.attachEvent?window.attachEvent("onload",d.bind(this,o)):window.addEventListener("load",d.bind(this,o),!1)}(window,document,"script","//cdn.stat-track.com/statics/moosend-tracking.min.js","mootrack");
+            //tracker has to be initialized otherwise it will generate warnings and wont sendtracking events
+            mootrack('init', '73180790077648839afcaaff1603f6ee');
+            mootrack('trackPageView');
+          `,
+        }}
+      />
+
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp
